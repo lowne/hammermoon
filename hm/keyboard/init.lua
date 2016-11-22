e0d56130b00686ca583ceb95f295f515d1ac309e
Mark Lowne <marklowne@gmail.com>
2016-11-22 13:15:38 +0100

    ✨ concurrent keypresses in hotkeys

--- hm/keyboard/init.lua
+++ hm/keyboard/init.lua
@@ -6,15 +6,14 @@
 local c=require'objc'
 local tolua,toobj,nptr=c.tolua,c.toobj,c.nptr
 c.load'Carbon.HIToolbox'
-local EVENTS=require'bridge.hitoolbox_events'
+--local EVENTS=require'bridge.hitoolbox_events'
 
 local next,pairs,ipairs=next,pairs,ipairs
 local sformat,tinsert=string.format,table.insert
 local bor,band=bit.bor,bit.band
-local getKeyCode,getFlags=require'hm._os.events'.getKeyCode,require'hm._os.events'.getFlags
 local now=require'hm.timer'.absoluteTime
 local coll=require'hm.types.coll'
-
+local keyEvent=require'hm._os.events'.key
 
 local activeContexts,enabledContexts={},{}
 
@@ -85,34 +84,43 @@ keyNames['✧']=HYPER_FLAG
 local keyOrder=getmetatable(keyboard.keys).order
 function ctx:newHotkey(keys,message)
   keys=keys:lower()
-  local separator
-  local codes,trigger={}
+  local prevSeparator
+  local codes,concurrent,trigger={},{}
   local d,again=1,true
   while again and d<#keys+1 do
-    print('looking in',keys:sub(d))
+    again=nil
+    --    print('looking in',keys:sub(d))
     for name,code in pairs(keyNames) do
       local s,e=keys:find(name,d,true)
       if s==d then
-        print('found',name)
+        --        print('found',name)
         if e==#keys then --last one
-          trigger=code
+          if prevSeparator=='+' then concurrent[code]=true
+          else trigger=code end
           d=#keys+1 again=true
           break
         else
           local next=keys:sub(e+1,e+1)
-          if next==' ' or next=='-' or next=='+' then
-            separator=separator or next
-            if next~=separator then error 'invalid format' end
-            codes[code]=true
+          --          print(next)
+          if next=='+' or next=='-' or next==' ' then
+            if next=='+' then
+              assert(code~=HYPER_FLAG,'invalid format')
+              concurrent[code]=true
+              prevSeparator=next
+            else
+              assert(prevSeparator~='+','invalid format')
+              codes[code]=true
+            end
+            prevSeparator=next
             d=e+2 again=true
             break
           end
         end
       end
     end
-    if not again then error'invalid format' end
+    assert(again,'invalid format')
   end
-  if d<=#keys then error'ha'end
+  assert(d>#keys,'invalid format?')
   if codes[HYPER_FLAG] then
     codes[HYPER_FLAG]=nil codes[keyCodes.cmd]=true codes[keyCodes.alt]=true codes[keyCodes.ctrl]=true codes[keyCodes.shift]=true
   end
@@ -120,33 +128,50 @@ function ctx:newHotkey(keys,message)
   for _,code in ipairs(keyOrder) do
     if codes[code] then idx=idx..(keySymbols[keyCodes[code]] or keyCodes[code]..'-') end
   end
-  for _,code in ipairs(keyOrder) do
-    if trigger==code then idx=idx..(keySymbols[keyCodes[code]] or keyCodes[code]) end
+  if next(concurrent) then
+    idx=idx..'['
+    for _,code in ipairs(keyOrder) do
+      if concurrent[code] then idx=idx..(keySymbols[keyCodes[code]] or keyCodes[code])..'+' end
+    end
+    idx=idx:sub(1,-2)..']'
+  else
+    for _,code in ipairs(keyOrder) do
+      if trigger==code then idx=idx..(keySymbols[keyCodes[code]] or keyCodes[code]) end
+    end
   end
   if not hotkeys[idx] then hotkeys[idx]=coll() end
   local msg=idx..(message and ': '..message or '')
-  local o=hk._new{_idx=idx,_msg=msg,_codes=codes,_trigger=trigger,_ctx=self,_isEnabled=false,_isActive=false}
+  print(msg)
+  local o=hk._new{_idx=idx,_msg=msg,_codes=codes,_trigger=trigger,_concurrent=next(concurrent) and concurrent,
+    _ctx=self,_isEnabled=false,_isActive=false}
   return o
 end
 
 local keycodesTree={}
+local currentBranch=keycodesTree
 local function rebuildTree()
   keycodesTree={}
-  --  local function copy(t)local r={} for k,v in pairs(t) do r[k]=v end return r end
   local function copybut(t,notk)local r={} for k,v in pairs(t) do if k~=notk then r[k]=v end end return r end
-  local function add(tree,codes,trigger,leaf)
-    if not next(codes) then tree[trigger]=leaf return end
-    for code in pairs(codes) do
-      tree[code]=tree[code] or {}
-      add(tree[code],copybut(codes,code),trigger,leaf)
+  local function add(tree,codes,trigger,concurrent,leaf)
+    if not next(codes) then
+      if concurrent then
+        for code in pairs(concurrent) do
+          add(tree,copybut(concurrent,code),code,nil,leaf)
+        end
+      elseif trigger then tree[trigger]=leaf return end
+    else
+      for code in pairs(codes) do
+        tree[code]=tree[code] or {}
+        add(tree[code],copybut(codes,code),trigger,concurrent,leaf)
+      end
     end
   end
   for ctx in pairs(activeContexts) do
     for hk in pairs(ctx._activeHotkeys) do
-      add(keycodesTree,hk._codes,hk._trigger,hk)
+      add(keycodesTree,hk._codes,hk._trigger,hk._concurrent,hk)
     end
   end
-  inspect(keycodesTree)
+  currentBranch=keycodesTree
 end
 
 local function hkActivate(self,batch,force)
@@ -201,49 +226,108 @@ function ctx:bind(keys,fn)
   return self:newHotkey(keys):enable():onPress(fn)
 end
 
+local CONCURRENT_KEYPRESSES_THRESHOLD=0.015
+local keyStates,OOB_FLAG,WAITRELEASE_FLAG={},{},{}
+local pendingKeys={}
 
-local keyStates,pendingKeys,OOB_FLAG={},{},{}
-local currentBranch=keycodesTree
+--local function emitOnePending(tmr)
+--  local code=pendingKeys[1]
+--  if code then
+--    print('emitting',keyCodes[code.down or code.up],code.down and 'down' or 'up')
+--    keyEvent(code.down or code.up,code.down):post()
+--  else tmr:cancel() end
+--  table.remove(pendingKeys,1)
+--end
+--local pendingTimer=require'hm.timer'.new(emitOnePending)
 
+--local function emitPending() if not pendingTimer.scheduled then return pendingTimer:runEvery(0.05,0.05) end end
+local function emitPending()
+  for _,code in ipairs(pendingKeys) do
+    log.v('[ >>]',code.down and '[PRS]' or '[REL]',keyCodes[code.down or code.up])
+    keyEvent(code.down or code.up,code.down):post()
+  end
+  pendingKeys={}
+end
+local max=math.max
 local function keyPressed(code)
-  print('pressed',keyCodes[code])
-  tinsert(pendingKeys,{down=code})
+  log.v('[PRS]',keyCodes[code])
+  if currentBranch.waitRelease then return true end --eat everything
   currentBranch=currentBranch[code]
-  --  inspect(currentBranch)
   if not currentBranch then
-    for _,code in ipairs(pendingKeys) do
-    --TODO emit codes
+    log.v('-----OOB')
+    emitPending()
+    currentBranch=OOB_FLAG
+    log.v('[ > ] [PRS]',keyCodes[code])
+    return
+  end
+  tinsert(pendingKeys,{down=code})
+  if currentBranch._concurrent then
+    print'concurrent!'
+    local last,delta=keyStates[code],0
+    for ccode in pairs(currentBranch._concurrent) do
+      print('a code is',ccode,last,keyStates[ccode])
+      delta=max(delta,last-assert(keyStates[ccode]))
     end
-    pendingKeys={} currentBranch=OOB_FLAG
-  else
-    if currentBranch.pressed then
-      currentBranch.pressed()
+    print(delta)
+    if delta>CONCURRENT_KEYPRESSES_THRESHOLD then
+      log.v('-----too slow, OOB')
+      emitPending()
+      currentBranch=OOB_FLAG
+      return true
+    else
+      if currentBranch.pressed then
+        currentBranch.pressed()
+      end
       pendingKeys={}
-      if not currentBranch.interstitial then currentBranch=OOB_FLAG end
+      currentBranch={waitRelease=code,released=currentBranch.released,_trigger=code}
+      log.v('-----waitRelease')
+      return true
+    end
+  end
+  if currentBranch.pressed then
+    currentBranch.pressed()
+    pendingKeys={}
+    if not currentBranch.interstitial then
+      currentBranch={waitRelease=code,released=currentBranch.released,_trigger=code}
+      log.v('-----waitRelease')
     end
   end
   return true
 end
 
 local function keyReleased(code)
-  print('released',keyCodes[code])
-  tinsert(pendingKeys,{up=code})
+  log.v('[REL]',keyCodes[code])
+  local willEat
+  if currentBranch.waitRelease then
+    if currentBranch.waitRelease~=code then return true
+    else willEat=true end
+  end
   if currentBranch.released and currentBranch._trigger==code then
-    currentBranch.released()
     pendingKeys={}
+    currentBranch.released()
+    willEat=true
+  elseif currentBranch~=OOB_FLAG and not currentBranch.waitRelease then
+    tinsert(pendingKeys,{up=code})
+    willEat=true
   end
   currentBranch=keycodesTree --restart from root now
   for k,v in pairs(keyStates) do
-    if v then
-      currentBranch=currentBranch[k] or OOB_FLAG
-    end
+    currentBranch=currentBranch[k] or OOB_FLAG
   end
-  --  inspect(currentBranch)
+  if currentBranch==OOB_FLAG or currentBranch==keycodesTree then
+    emitPending()
+  end
+  if not willEat then
+    log.v('[ > ] [REL]',keyCodes[code])
+  end
+  if currentBranch==OOB_FLAG then log.v('-----OOB')
+  elseif currentBranch==keycodesTree then log.v('READY') end
+  return willEat
 end
 local function keyRepeated(code)
-  --  print('repeated',keyCodes[code])
-  if currentBranch.repeated then currentBranch.repeated()
-  else tinsert(pendingKeys,{down=code}) end
+  if currentBranch==OOB_FLAG then return
+  elseif currentBranch.repeated then currentBranch.repeated() return true
+  else tinsert(pendingKeys,{down=code}) return true end
 end
 
 local callback={
@@ -254,14 +338,14 @@ local callback={
   end,
   keyUp=function(ev)
     local code=ev:getKeyCode()
-    keyStates[code]=false return keyReleased(code)
+    keyStates[code]=nil return keyReleased(code)
   end,
   flagsChanged=function(ev)
-    for key,state in pairs(ev:getFlags()) do
+    for key,state in pairs(ev:getMods()) do
       local code=keyCodes[key]
       if not state~=not keyStates[code] then
         if state then keyStates[code]=now() return keyPressed(code)
-        else keyStates[code]=false return keyReleased(code) end
+        else keyStates[code]=nil return keyReleased(code) end
       end
     end
   end,
